a7bb2fbf0d0931564bb737508ef7239c510b0f82
Ash786Sharma <ash786sharma@gmail.com>
2025-01-13 20:46:30 +0530

    Changes for Vercel Deployment of App_Backend

--- Flutter_App_QuickAttend/lib/Utils/constants.dart
+++ Flutter_App_QuickAttend/lib/Utils/constants.dart
@@ -1,3 +1,4 @@
 //const String baseUrl = 'http://10.0.2.2:5000'; //for emulator
 //const String baseUrl = 'http://localhost:5000'; //For WEB
-const String baseUrl = 'https://quick-attend.loca.lt'; //For localTunnel
+//const String baseUrl = 'https://quick-attend.loca.lt'; //For localTunnel
+const String baseUrl = 'https://quick-attend-app.vercel.app'; //For localTunnel
